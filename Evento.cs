af76751381dd67982ed55386130c4175011fc8ff
RubaCodes <rubacodes@gmail.com>
2022-09-26 09:59:15 +0100

    creato costruttore + eccezioni

--- Evento.cs
+++ Evento.cs
@@ -1,15 +1,63 @@
 ﻿// See https://aka.ms/new-console-template for more information
 public class Evento {
-    public string Titolo { get; set; }
-    public DateOnly Data { get; set; }
-
-    public int CapienzaMax {get; private set;}
-    public int PostiPrenotati { get; private set; }
-
-    public Evento(string title,DateOnly data, int capienza) {
-        Titolo = title;
-        Data = data;
-        CapienzaMax = capienza;
-        PostiPrenotati = 0;
+    public string Titolo { 
+        get {
+            return Titolo;
+        } 
+        set {
+            if (Titolo == "") {
+                throw new EmptyTitle("Il Titolo inserito non puo' essere vuoto");
+            }
+            Titolo = value;
+        } 
     }
+    public DateTime Data {
+        get {
+            return Data;
+        }
+        set {
+            if (Data < DateTime.Now) {
+                throw new DateExeption("La data deve essere successiva al data odierna");
+            }
+            Data = value;
+        } 
+    }
+
+    public int PostiPrenotati {get; private set;}
+    public int CapienzaMax
+    { 
+        get {
+            return CapienzaMax;
+            } 
+        private set {
+            if (CapienzaMax < 1) {
+                throw new CapienzaMin("La Capienza massima dell' evento deve essere maggiore di 0"); 
+            }
+            CapienzaMax = value;
+        } 
+    }
+
+    public Evento(string title,DateTime data, int capienza) {
+
+            Titolo = title;
+            Data = data;
+            CapienzaMax = capienza;
+            PostiPrenotati = 0;
+    }
+}
+
+
+/*
+ * Eccezioni
+ */
+public class EmptyTitle : Exception {
+    public EmptyTitle(string message) : base(message) { }
+}
+public class CapienzaMin : Exception
+{
+    public CapienzaMin(string message) : base(message) { }
+}
+public class DateExeption : Exception
+{
+    public DateExeption(string message) : base(message) { }
 }
