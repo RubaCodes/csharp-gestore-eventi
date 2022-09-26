ba213509eb104875682b0e32b589c52fa8fded3a
RubaCodes <rubacodes@gmail.com>
2022-09-26 09:21:52 +0100

    creazione iniziale costruttore

---
+++ Evento.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Evento {
+    public string Titolo { get; set; }
+    public DateOnly Data { get; set; }
+
+    public int CapienzaMax {get; private set;}
+    public int PostiPrenotati { get; private set; }
+
+    public Evento(string title,DateOnly data, int capienza) {
+        Titolo = title;
+        Data = data;
+        CapienzaMax = capienza;
+        PostiPrenotati = 0;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-
-public class Evento {
-    public string Titolo { get; set; }
-    public DateOnly Data { get; set; }
-
-    public int CapienzaMax {get; private set;}
-    public int PostiPrenotati { get; private set; }
-}
