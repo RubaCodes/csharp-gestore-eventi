c2b9623812500fd28447d444f1a7a722cd3d7d29
RubaCodes <rubacodes@gmail.com>
2022-09-26 10:41:58 +0100

    inizializzazione ProgrammaEventi

---
+++ ProgrammaEventi.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+
+
+
+/*
+ * Eccezioni
+ */
+/*
+ * eccezioni sui metodi
+ */
+public class ProgrammaEventi {
+    public string Titolo { get; set; }
+    public List<Evento> eventi;
+
+     public ProgrammaEventi(string titolo) {
+        Titolo = titolo;
+        eventi = new List<Evento>();
+    }
+}
